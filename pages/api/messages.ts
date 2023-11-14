2ddedb912ea425451892ac87a0df2c160354afef
John Parker <john.henry.parker@gmail.com>
2023-11-13 20:37:14 -0500

    avoid writing after send res

--- pages/api/messages.ts
+++ pages/api/messages.ts
@@ -12,23 +12,25 @@ export default function handler(
 
   const verify_token = process.env.FB_VERIFY_TOKEN;
   switch (method) {
-    case 'GET':
+    case 'GET': {
       if (mode && token) {
         // Check the mode and token sent are correct
         if (mode === "subscribe" && token === verify_token) {
           // Respond with 200 OK and challenge token from the request
           console.log("WEBHOOK_VERIFIED");
-          res.status(200).send(challenge);
+          return res.status(200).send(challenge);
         } else {
           // Responds with '403 Forbidden' if verify tokens do not match
-          res.status(403);
+          return res.status(403);
         }
       }
-    case 'POST':
+    }
+    case 'POST': {
       //return handlePost(req, res);
-      res.status(200)
-    default:
-      res.setHeader('Allow', ['GET', 'POST'])
-      res.status(405).end(`Method ${method} Not Allowed`)
+      return res.status(200)
+    }
+    default: {
+      return res.setHeader('Allow', ['GET', 'POST']).status(405).end(`Method ${method} Not Allowed`)
+    }
   }
 }
