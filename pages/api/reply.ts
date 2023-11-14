cd0cbf48fe2539317d8c4dfd61c16dd055abf112
John Parker <john.henry.parker@gmail.com>
2023-11-14 10:38:46 -0500

    Add additional return status 200

--- pages/api/reply.ts
+++ pages/api/reply.ts
@@ -86,6 +86,7 @@ export default async function handler(
                 res.json({ success: true });
                 return res.status(200).end()
             }
+            return res.status(200).end()
         }
         default: {
             res.setHeader('Allow', ['GET', 'POST'])
