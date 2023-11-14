f8ac212d62c5fd34ecc7e0da413f495b44c19f75
John Parker <john.henry.parker@gmail.com>
2023-11-14 09:27:48 -0500

    First working version

--- pages/api/messages.ts
+++ pages/api/messages.ts
@@ -1,16 +1,40 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from "axios";
+import OpenAI from 'openai';
+import { Redis } from '@upstash/redis'
 
-export default function handler(
+// Assert that required environment variables are provided
+if (!process.env.OPENAI_API_KEY || !process.env.UPSTASH_TOKEN || !process.env.REDIS_URL || !process.env.REDIS_TOKEN || !process.env.ASSISTANT_ID || !process.env.BASE_URL) {
+  throw new Error('Required environment variables are missing');
+}
+
+// OpenAI configuration
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY!,
+});
+
+// Redis configuration
+const redis = new Redis({
+  url: process.env.REDIS_URL!,
+  token: process.env.REDIS_TOKEN!,
+})
+
+// Other constants
+const assistant_id = process.env.ASSISTANT_ID!;
+const base_url = process.env.BASE_URL!;
+const upstash_token = process.env.UPSTASH_TOKEN!;
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { method, query } = req;
-  let mode = req.query["hub.mode"];
-  let token = req.query["hub.verify_token"];
-  let challenge = req.query["hub.challenge"];
-
   const verify_token = process.env.FB_VERIFY_TOKEN;
+  const { method, query, body } = req;
+
+  let mode = query["hub.mode"];
+  let token = query["hub.verify_token"];
+  let challenge = query["hub.challenge"];
+
   switch (method) {
     case 'GET': {
       if (mode && token) {
@@ -24,13 +48,81 @@ export default function handler(
           return res.status(403);
         }
       }
+      else {
+        const thread = await openai.beta.threads.create();
+        console.log(thread);
+      }
     }
     case 'POST': {
-      //return handlePost(req, res);
-      return res.status(200)
+      if (body.object) {
+        if (
+          body.entry &&
+          body.entry[0].changes &&
+          body.entry[0].changes[0] &&
+          body.entry[0].changes[0].value.messages &&
+          body.entry[0].changes[0].value.messages[0]
+        ) {
+          console.log(JSON.stringify(body, null, 2));
+          const phone_number_id =
+            body.entry[0].changes[0].value.metadata.phone_number_id;
+          const user: any = await redis.get(phone_number_id);
+          let thread_id = "";
+          // Create a new thread if user is not found
+          if (!user || !user.thread_id) {
+            const thread = await openai.beta.threads.create();
+            await redis.set(phone_number_id, { thread_id: thread.id });
+            thread_id = thread.id;
+          }
+          else {
+            thread_id = user.thread_id;
+          }
+
+          const from = body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+          const msg_body = body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+          const name = body.entry[0].changes[0].value.contacts[0].profile.name;
+
+          const message = await openai.beta.threads.messages.create(
+            thread_id,
+            {
+              role: "user",
+              content: msg_body
+            }
+          );
+          const run = await openai.beta.threads.runs.create(
+            thread_id,
+            {
+              assistant_id: assistant_id,
+              //instructions: `El nombre del usuario es: ${name}`
+            }
+          );
+          console.log(run);
+          axios({
+            method: "POST",
+            url:
+              "https://qstash.upstash.io/v2/publish/" +
+              base_url + "/api/reply",
+            data: {
+              phone_number_id,
+              to: from.replace(/^521/, "52"),
+              run,
+            },
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer " + upstash_token,
+              "Upstash-Delay": "2s",
+            },
+          }).catch((error) => {
+            console.log("error", error);
+          });
+        }
+        res.json({ success: true });
+      } else {
+        res.status(404);
+      }
     }
     default: {
-      return res.setHeader('Allow', ['GET', 'POST']).status(405).end(`Method ${method} Not Allowed`)
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
     }
   }
 }
---
+++ pages/api/reply.ts
@@ -0,0 +1,93 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import OpenAI from 'openai';
+import axios from "axios";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const base_url = process.env.BASE_URL;
+const upstash_token = process.env.UPSTASH_TOKEN;
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    const wa_token = process.env.WHATSAPP_TOKEN;
+
+    const { method, query, body } = req;
+
+    switch (method) {
+        case 'POST': {
+            console.log(JSON.stringify(body, null, 2))
+            const phone_number_id = body.phone_number_id
+            const to = body.to
+            const run = await openai.beta.threads.runs.retrieve(
+                body.run.thread_id,
+                body.run.id
+            );
+            console.log(run);
+            if (run.status === "queued" || run.status === "in_progress") {
+                axios({
+                    method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+                    url:
+                        "https://qstash.upstash.io/v2/publish/" +
+                        base_url + "/api/reply",
+                    data: {
+                        phone_number_id,
+                        to: to,
+                        run,
+                    },
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + upstash_token,
+                        "Upstash-Delay": "1s",
+                    },
+                }).catch((error) => {
+                    console.log("error", error);
+                });
+            }
+            else if (run.status === "completed") {
+                const messages = await openai.beta.threads.messages.list(
+                    run.thread_id
+                );
+                const run_assistant_messages = messages.data.filter(
+                    (message) => message.role === "assistant" && message.run_id === run.id
+                );
+                console.log(run_assistant_messages)
+                for (let i = 0; i < run_assistant_messages.length; i++) {
+                    const message = run_assistant_messages[i]
+                    console.log(JSON.stringify(message, null, 2))
+                    const content = message.content[0];
+                    // Filter content from message to avoid MessageContentImageFile
+                    if (content.type === "text") {
+                        axios({
+                            method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+                            url:
+                                "https://graph.facebook.com/v17.0/" +
+                                phone_number_id +
+                                "/messages",
+                            data: {
+                                messaging_product: "whatsapp",
+                                to: to,
+                                text: { body: content.text.value },
+                            },
+                            headers: {
+                                "Content-Type": "application/json",
+                                "Authorization": "Bearer " + wa_token,
+                            },
+                        }).catch((error) => {
+                            console.log("error", error);
+                        });
+                    }
+                }
+            }
+            res.json({ success: true });
+        }
+        default: {
+            res.setHeader('Allow', ['GET', 'POST'])
+            res.status(405).end(`Method ${method} Not Allowed`)
+        }
+    }
+}
