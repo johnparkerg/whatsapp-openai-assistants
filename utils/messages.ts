ab2b00bfbd4368c56d6aefbbfc7cabdb5b39521b
John Parker <john.henry.parker@gmail.com>
2023-12-19 09:08:52 -0500

    Only clean phones if present

--- utils/messages.ts
+++ utils/messages.ts
@@ -18,12 +18,20 @@ type Message = z.infer<typeof MessageSchema>;
 const saveMessage = async (message: Message) => {
     // Save message to database using Prisma
     const prisma = new PrismaClient();
+    let clean_sender_phone = message.sender_phone;
+    if (message.sender_phone && message.sender_phone.startsWith("521")) {
+        clean_sender_phone = message.sender_phone.replace(/^521/, "52");
+    }
+    let clean_receiver_phone = message.receiver_phone;
+    if (message.receiver_phone && message.receiver_phone.startsWith("521")) {
+        clean_receiver_phone = message.receiver_phone.replace(/^521/, "52");
+    }
     await prisma.messages.create({
         data: {
             message_id: message.message_id,
             user_id: message.user_id,
-            sender_phone: message.sender_phone.replace(/^521/, "52"),
-            receiver_phone: message.receiver_phone.replace(/^521/, "52"),
+            sender_phone: clean_sender_phone,
+            receiver_phone: clean_receiver_phone,
             message_text: message.message_text,
             raw_message: message.raw_message,
             created_on: message.created_on
